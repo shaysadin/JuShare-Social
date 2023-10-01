ace94e1443839448b4a244919a25fb2db0d01c84
shaysadin <shay.sadina29@gmail.com>
2023-10-02 00:28:07 +0300

    small fixess

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,18 +4,4 @@ module.exports = {
     watch: '.'
   }],
 
-  deploy : {
-    production : {
-      key: 'faeshare-server-key.pem',
-      user : 'ubuntu',
-      host : '54.236.48.80',
-      ref  : 'origin/main',
-      repo : 'git@github.com:Jagjot26/faeshare.git',
-      path : '/home/ubuntu',
-      'pre-deploy-local': '',
-      'post-deploy' : 'source ~/.nvm/nvm.sh && npm install --legacy-peer-deps && npm run build && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': '',
-      'ssh_options': 'ForwardAgent=yes'
-    }
-  }
 };
